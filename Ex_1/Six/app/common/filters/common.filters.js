10ab19456b3f09011558ab913044c7840e0b2efa
mayuresh <mayuresh@gmail.com>
2015-07-16 13:03:14 +0530

    Added new filters

--- Ex_1/Six/app/common/filters/common.filters.js
+++ Ex_1/Six/app/common/filters/common.filters.js
@@ -3,4 +3,17 @@ angular.module('common')
         return function (text) {
             return text.toString().toUpperCase();
         }
+    })
+    .filter('genericStringFilter', function() {
+        return function (nameList, arg) {
+            var filteredList = [];
+
+            for (var i = 0; i < nameList.length; i++) {
+                if (nameList[i].toString().indexOf(arg) != -1) {
+                    filteredList.push(nameList[i]);
+                }
+            }
+
+            return filteredList;
+        }
     });
--- Ex_1/Six/app/name/filters/name.filters.js
+++ Ex_1/Six/app/name/filters/name.filters.js
@@ -1,6 +1,6 @@
 angular.module('name')
-.filter('planetFilter', function() {
-        return function(nameList) {
+    .filter('planetFilter', function () {
+        return function (nameList) {
             var filteredList = [];
             var planets = ['Mercury', 'Venus', 'Earth', 'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune'];
 
