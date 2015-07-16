b6aacd1b20a8fca944540796d876326b8fa8f011
mayuresh <mayuresh@gmail.com>
2015-07-16 14:37:59 +0530

    Added filter with parameter

--- Ex_1/Six/app/common/filters/common.filters.js
+++ Ex_1/Six/app/common/filters/common.filters.js
@@ -1,15 +1,15 @@
 angular.module('common')
-    .filter('capitaliseFilter', function () {
+    .filter('capitaliseFilter', function () { // Convert string to uppercase
         return function (text) {
             return text.toString().toUpperCase();
         }
     })
-    .filter('genericStringFilter', function() {
-        return function (nameList, arg) {
+    .filter('genericStringFilter', function() { // Filter list to only retain strings containing the string in filterString
+        return function (nameList, filterString) {
             var filteredList = [];
 
             for (var i = 0; i < nameList.length; i++) {
-                if (nameList[i].toString().indexOf(arg) != -1) {
+                if (nameList[i].toString().indexOf(filterString) != -1) {
                     filteredList.push(nameList[i]);
                 }
             }
