33edee54bcdb6769c62853302e94d8f312afef36
mayuresh <mayuresh@gmail.com>
2015-07-23 10:20:31 +0530

    Pulled changes from Master

---
+++ Ex_1/Seven/app/name/directives/name.directive.js
@@ -0,0 +1,6 @@
+angular.module('name')
+    .directive('nameList', function () {
+        return {
+            templateUrl: 'app/name/templates/name_list_template.html'
+        }
+    });
--- README.md
+++ README.md
@@ -1,6 +1,2 @@
 # js_training_2
 Repository to use for JavaScript training - Batch 2
-<<<<<<< HEAD
-This is gh-pages
-=======
->>>>>>> 7af62425c1763f79ff72ea184434a77a530f3dbe
